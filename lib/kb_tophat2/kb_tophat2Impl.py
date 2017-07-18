75693ad909d2a4fbb67c8af655416fe62028e67c
Tianhao-Gu <tgu@anl.gov>
2017-07-18 14:45:01 -0500

    update default suffix

--- lib/kb_tophat2/kb_tophat2Impl.py
+++ lib/kb_tophat2/kb_tophat2Impl.py
@@ -24,7 +24,7 @@ class kb_tophat2:
     ######################################### noqa
     VERSION = "1.0.0"
     GIT_URL = "https://github.com/Tianhao-Gu/kb_tophat2.git"
-    GIT_COMMIT_HASH = "9cadfd7e7b0a7101f1daf5642812d289bf0ab1da"
+    GIT_COMMIT_HASH = "33ce371902892b989c5de023a3bcd1eeacff89a5"
 
     #BEGIN_CLASS_HEADER
     #END_CLASS_HEADER
